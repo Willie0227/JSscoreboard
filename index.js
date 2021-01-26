c78520147d1fc6e8e92b886162db3d03b4f72209
Willie0227 <72426737+Willie0227@users.noreply.github.com>
2021-01-26 21:38:02 +0800

    bug fix

--- index.js
+++ index.js
@@ -1,29 +1,35 @@
 let vars = {
     homePoints: 0,
     guestPoints: 0,
-    periods:0,
-    name1:"",
-    name2:"",
-    name3:"",
-    name4:""
+    periods: 0,
+    name1: "",
+    name2: "",
+    name3: "",
+    name4: ""
 }
 
 function addPT(selector, name) {
+    if (name === 'periods' && vars.periods === 3)
+        return;
     vars[name]++;
     document.querySelector(selector).innerHTML = vars[name]
 }
 
 function deductPT(selector, name) {
+    if (vars[name] === 0) {
+        return;
+    }
     vars[name]--;
     document.querySelector(selector).innerHTML = vars[name]
+
 }
 
-function addName(selector,name) {
+function addName(selector, name) {
     vars [name] = prompt("Please enter the player's name")
     document.querySelector(selector).innerHTML = vars[name]
 }
 
-function removeName(selector,name) {
+function removeName(selector, name) {
     vars[name] = ""
     document.querySelector(selector).innerHTML = vars[name]
 }
@@ -32,16 +38,16 @@ function reset() {
     vars = {
         homePoints: 0,
         guestPoints: 0,
-        periods:0,
-        name1:"",
-        name2:"",
-        name3:"",
-        name4:""
+        periods: 0,
+        name1: "",
+        name2: "",
+        name3: "",
+        name4: ""
     }
 
     document.querySelector('.home').innerHTML = vars ["homePoints"]
     document.querySelector('.guest').innerHTML = vars["guestPoints"]
-    document.querySelector('.periodBox').innerHTML= vars ["periods"]
+    document.querySelector('.periodBox').innerHTML = vars ["periods"]
     document.querySelector('.person1').innerHTML = vars["name1"]
     document.querySelector('.person2').innerHTML = vars["name2"]
     document.querySelector('.person3').innerHTML = vars["name3"]
